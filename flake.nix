d61108942f21129685fedbadbc4ca743aa9ac239
Ushitora Anqou <ushitora@anqou.net>
2026-01-03 11:19:39 +0900

    add flake.nix for local development

---
+++ flake.nix
@@ -0,0 +1,116 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    opam-nix = {
+      url = "github:tweag/opam-nix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+        opam-repository.follows = "opam-repository";
+      };
+    };
+
+    opam-repository = {
+      url = "github:ocaml/opam-repository";
+      flake = false;
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    opam-nix,
+    opam-repository,
+  } @ inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        package = "merlint";
+        pkgs = nixpkgs.legacyPackages.${system};
+        lib = nixpkgs.lib;
+        on = opam-nix.lib.${system};
+        devPackagesQuery = {
+          ocaml-lsp-server = "*";
+          utop = "*";
+          ocamlformat = let
+            # read .ocamlformat
+            # cf. https://nymphium.github.io/2023/05/06/purely-functioinal-ocaml-development.html
+            ocamlformatConfig = lib.strings.splitString "\n" (builtins.readFile ./.ocamlformat);
+            re = builtins.match "version[[:space:]]*=[[:space:]]*([^[:space:]]*)[[:space:]]*$";
+            versionLine =
+              lib.lists.findFirst
+              (l: builtins.isList (re l))
+              (throw "no version specified in .ocamlformat")
+              ocamlformatConfig;
+            version = builtins.elemAt (re versionLine) 0;
+          in
+            version;
+        };
+        query =
+          devPackagesQuery
+          // {
+            ## You can force versions of certain packages here, e.g:
+            ## - force the ocaml compiler to be taken from opam-repository:
+            ocaml-base-compiler = "*";
+            ## - or force the compiler to be taken from nixpkgs and be a certain version:
+            #ocaml-system = "*";
+            ## - or force ocamlfind to be a certain version:
+            #ocamlfind = "1.9.6";
+          };
+        src = ./.;
+        scope =
+          on.buildOpamProject' {
+            repos = [opam-repository];
+            resolveArgs = {
+              with-test = true;
+              with-doc = true;
+            };
+          }
+          src
+          query;
+        overlay = final: prev:
+          (
+            # disable library's test and doc
+            # cf. https://nymphium.github.io/2023/05/06/purely-functioinal-ocaml-development.html
+            with builtins;
+              mapAttrs
+              (p: _:
+                if hasAttr "passthru" prev.${p} && hasAttr "pkgdef" prev.${p}.passthru
+                then
+                  prev.${p}.overrideAttrs (_: {
+                    opam__with_test = "false";
+                    opam__with_doc = "false";
+                  })
+                else prev.${p})
+              prev
+          )
+          // {
+            # You can add overrides here
+            ${package} = prev.${package}.overrideAttrs (_: {
+              # Prevent the ocaml dependencies from leaking into dependent environments
+              doNixSupport = false;
+              removeOcamlReferences = true;
+              buildInputs = with pkgs; [];
+            });
+          };
+        scope' = scope.overrideScope overlay;
+        # The main package containing the executable
+        main = scope'.${package};
+        # Packages from devPackagesQuery
+        devPackages = builtins.attrValues (pkgs.lib.getAttrs (builtins.attrNames devPackagesQuery) scope');
+      in {
+        formatter = pkgs.alejandra;
+
+        legacyPackages = scope';
+
+        packages.default = main;
+
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [main];
+          buildInputs = devPackages;
+        };
+      }
+    );
+}
